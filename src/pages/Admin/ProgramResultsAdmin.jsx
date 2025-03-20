217cd2215819f0bd55f6eb34a7af4a390d88a247
Saleel10 <salisaleel2004@gmail.com>
2025-03-20 06:37:21 +0530

    2

--- src/pages/Admin/ProgramResultsAdmin.jsx
+++ src/pages/Admin/ProgramResultsAdmin.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { collection, getDocs, addDoc, query, where, doc, getDoc, updateDoc } from 'firebase/firestore';
+// import { db } from '@/config/firebase';
 import { db } from '@/config/firebase';
 
 
